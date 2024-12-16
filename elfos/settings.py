2c64b37d5cb779b2694a0a67cb859a8bb8cb16af
Stephen Bevan <mrsteverox@gmail.com>
2024-12-16 23:17:22 +0000

    updated allowed host to inclued  heroku

--- elfos/settings.py
+++ elfos/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-stephenbevan-elfos-m1wo3xb9nv2.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-stephenbevan-elfos-m1wo3xb9nv2.ws.codeinstitute-ide.net',
+'.herokuapp.com',]
 
 CSRF_TRUSTED_ORIGINS = [
     'https://*.codeinstitute-ide.net',
