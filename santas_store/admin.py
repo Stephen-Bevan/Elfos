249fee59e235de3848a457b1b6e521f0ee6526d0
Yaroslav Matala <s9682642@gmail.com>
2024-12-19 13:53:09 +0000

    implement backend for santas letter

--- santas_store/admin.py
+++ santas_store/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import Letter
 
-@admin.register(Letter)
-class LetterAdmin(admin.ModelAdmin):
-    list_display = ('name', 'title', 'creation_date', 'user')
-    search_fields = ('name', 'title', 'text')
+admin.site.register(Letter)
+
+# @admin.register(Letter)
+# class LetterAdmin(admin.ModelAdmin):
+#     list_display = ('name', 'title', 'creation_date', 'user')
+#     search_fields = ('name', 'title', 'text')
--- santas_store/urls.py
+++ santas_store/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from .views import HomePage, letter_list, letter_detail
 
+from . import views
+
+
+
 urlpatterns = [
     path('', HomePage.as_view(), name='home'),  # Home Page
     path('letters/', letter_list, name='letter_list'),  # List of Letters
     path('letters/<int:pk>/', letter_detail, name='letter_detail'),  # Detail View for a Letter
+    path("write-to-santa/", views.write_to_santa, name="write_to_santa"),
+    path("congratulations/", views.congratulations, name="congratulations"),
 ]
